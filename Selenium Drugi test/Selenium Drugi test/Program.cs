8b21dc515ca7e14e7cc9370c6e5fb67de6d596af
QA36 <QA36@DESKTOP-QPQFVHR>
2022-11-10 21:07:23 +0100

    init

---
+++ Selenium Drugi test/Selenium Drugi test/Program.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium_Drugi_test
+{
+    public class Program
+    {
+
+        IWebDriver driver = new ChromeDriver();
+        static void Main(string[] args)
+        {
+
+
+
+        }
+
+        [SetUp]
+        public void Inicijalizacija()
+        {
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            IWebElement userName = driver.FindElement(By.Id("user-name"));
+            userName.SendKeys("standard_user");
+
+            IWebElement password = driver.FindElement(By.Id("password"));
+            password.SendKeys("secret_sauce");
+
+            IWebElement loginButton = driver.FindElement(By.Id("login-button"));
+            loginButton.Submit();
+
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+    }
+}
+
+
+
---
+++ Selenium prvi test/Selenium prvi test/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_prvi_test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://www.google.com/");
+
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            element.SendKeys("Selenium");
+
+            driver.Close();
+            
+
+
+        }
+    }
+}
